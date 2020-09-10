8598f436a7192ec85ac1a635e307236d879f6649
Arielle Scherr <ariellescherr@gmail.com>
2020-09-10 17:05:08 -0500

    created two tables, added data, practiced queries

--- server/schema.sql
+++ server/schema.sql
@@ -3,13 +3,20 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  txt VARCHAR(50) NOT NULL,
+  roomname VARCHAR(20) NOT NULL,
+  userid INT NOT NULL,
+  PRIMARY KEY (id)
+
+);
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
